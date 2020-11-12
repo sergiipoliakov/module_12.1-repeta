cb5d5aa284309667ec50301ad1ac553af3779d22
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-11-12 18:11:16 +0200

    add

--- src/js/01-fetch-api.js
+++ src/js/01-fetch-api.js
@@ -31,7 +31,9 @@ function onSearch(e) {
   API.fetchPokemon(searchQuery)
     .then(renderPokemonCard)
     .catch(onFetchError)
-    .finally(() => form.reset());
+    .finally(() => {
+      form.reset();
+    });
 }
 
 function renderPokemonCard(pokemon) {
@@ -39,19 +41,48 @@ function renderPokemonCard(pokemon) {
   refs.cardContainer.innerHTML = markup;
 }
 
-function onFetchError(error) {
-  alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
+function onFetchError() {
+  alert('все плохо!!');
 }
+
+// import pokemonCardTpl from '../templates/pokemon-card.hbs';
+// import API from './api-service';
+// import getRefs from './get-refs';
+
+// const refs = getRefs();
+
+// refs.searchForm.addEventListener('submit', onSearch);
+
+// function onSearch(e) {
+//   e.preventDefault();
+
+//   const form = e.currentTarget;
+//   const searchQuery = form.elements.query.value;
+
+//   API.fetchPokemon(searchQuery)
+//     .then(renderPokemonCard)
+//     .catch(onFetchError)
+//     .finally(() => form.reset());
+// }
+
+// function renderPokemonCard(pokemon) {
+//   const markup = pokemonCardTpl(pokemon);
+//   refs.cardContainer.innerHTML = markup;
+// }
+
+// function onFetchError(error) {
+//   alert('Упс, что-то пошло не так и мы не нашли вашего покемона!');
+// }
 
 // =========================================
 
-const url = 'https://newsapi.org/v2/everything?q=cars';
-const options = {
-  headers: {
-    Authorization: '4330ebfabc654a6992c2aa792f3173a3',
-  },
-};
+// const url = 'https://newsapi.org/v2/everything?q=cars';
+// const options = {
+//   headers: {
+//     Authorization: '4330ebfabc654a6992c2aa792f3173a3',
+//   },
+// };
 
-fetch(url, options)
-  .then(r => r.json())
-  .then(console.log);
+// fetch(url, options)
+//   .then(r => r.json())
+//   .then(console.log);
